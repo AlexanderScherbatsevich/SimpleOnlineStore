e27bd29aa500645b72cf606960627cc56e537678
Aleksandr <alexscherb91@gmail.com>
2022-09-02 15:12:23 +0300

    Update cart
    added remove button
    create ModelBinder

---
+++ SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            //получить объект Cart из сессии
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+
+            if(cart == null)
+            {
+                cart =  new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            return cart;
+        }
+    }
+}
